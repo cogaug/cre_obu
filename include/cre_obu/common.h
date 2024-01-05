ca5ead0ee8c9cac3f25412192d96e0dc8a2a5101
albert <albert@it-telecom.co.kr>
2024-01-05 17:50:09 +0900

    add common header

---
+++ include/cre_obu/common.h
@@ -0,0 +1,25 @@
+/**
+ * @file    common.h
+ * @author  albert(albert@it-telecom.co.kr)
+ * @date    2024-01-02
+ * @brief   common header
+ * @version 1.0
+ */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __COMMON_H__ */
