c8634bf264e76c875fc3906acc5dad51b0754113
albert <albert@it-telecom.co.kr>
2024-01-11 11:06:38 +0900

    uds 내부통신 구현

--- src/cre_obu/cre_obu.c
+++ src/cre_obu/cre_obu.c
@@ -2,13 +2,84 @@
 #include <cre_obu.h>
 #include "version.h"
 
-int main()
+/**
+ * @brief       send data to v2x using uds
+ * @param[in]   buffer : 전달할 데이터
+ * @param[in]   len : 전달할 데이터의 크기
+ * @retval      none
+ * @author      albert
+ * @date        2024-01-09
+ */
+void acu_send_to_v2x(unsigned char *buffer, int len)
 {
-    /* threads pid */
-    pthread_t pid[2];
+    int sock = -1;
+    struct sockaddr_un addr_un;
+
+    sock = socket(AF_UNIX, SOCK_DGRAM, 0);
+
+    /* socket create error */
+    if (sock < 0) {
+        return;
+    }
+
+    memset(&addr_un, 0x00, sizeof(struct sockaddr_un));
+    addr_un.sun_family = AF_UNIX;
+    strcpy(addr_un.sun_path, UDS_FILE_ACU_TX);
+
+    sendto(sock, buffer, len, 0, (struct sockaddr *)&addr_un, sizeof(struct sockaddr_un));
+
+    close(sock);
+} /* acu_send_to_v2x */
+            acu_send_to_v2x(buffer, O2A_HEADER_SIZE + length);
+    int uds_sock = -1;
+    struct sockaddr_un msg_addr_un;
+    struct sockaddr_un msg_recv_un;
+    int namelen = sizeof(struct sockaddr_un);
     int retval = 0;
 
-    printf("hello world\n");
+    /* uds server open */
+    uds_sock = socket(AF_UNIX, SOCK_DGRAM, 0);
+
+    /* uds socket create error */
+    if (uds_sock < 0) {
+        sleep(1);
+        goto client_tcp_exit;
+    }
+
+    memset(&msg_addr_un, 0x00, sizeof(struct sockaddr_un));
+    msg_addr_un.sun_family = AF_UNIX;
+    strcpy(msg_addr_un.sun_path, UDS_FILE_ACU_RX);
+
+    if (uds_sock < 0) {
+        sleep(1);
+        goto client_tcp_exit;
+    }
+
+    if (bind(uds_sock, (struct sockaddr *) &msg_addr_un, sizeof(struct sockaddr_un)) < 0) {
+        sleep(1);
+        goto client_uds_exit;
+    }
+
+
+    /* set uds socket nonblocking */
+    if ((fl = fcntl(uds_sock, F_GETFL, NULL)) == -1) {
+        sleep(1);
+        goto client_uds_exit;
+    }
+
+    fl |= O_NONBLOCK;
+
+    if (fcntl(uds_sock, F_SETFL, fl) == -1) {
+        sleep(1);
+        goto client_uds_exit;
+    }
+        /* recv from v2x */
+        retval = recvfrom(uds_sock, rx_buffer, sizeof(rx_buffer), 0, (struct sockaddr *)&msg_recv_un, (socklen_t*)&namelen);
+
+    /* uds close */
+    if (uds_sock > 0) {
+        close(uds_sock);
+    }
 
     /* create v2x_thread */
     retval = pthread_create(&pid[0], NULL, v2x_thread, NULL);
--- src/cre_obu/v2x_interface.c
+++ src/cre_obu/v2x_interface.c
@@ -11,32 +11,33 @@
 #include "lte_api.h"
 
 /**
- * @brief       SPDU를 생성하여 반환한다.
- * @param[in]   psid : 송신 PSID
- * @param[in]   dataunit :  payload
- * @param[in]   dataunit_len : payload 길이
- *
- * @retval      apiResult
- *
+ * @brief       send data to acu inteface using uds
+ * @param[in]   buffer : 전달할 데이터
+ * @param[in]   len : 전달할 데이터의 크기
+ * @retval      none
  * @author      albert
- * @date        2024-01-03
+ * @date        2024-01-09
  */
-apiResult v2x_send_spdu(lte_dot3PsidRange psid, unsigned char *dataunit, int dataunit_len)
+void v2x_send_to_acu(unsigned char *buffer, int len)
 {
-    apiResult result = ar_success;
-    unsigned char dest_mac[6] = { 0xff };
+    int sock = -1;
+    struct sockaddr_un addr_un;
 
-    result = lteUnsignedWsmTransmit(psid, lte_power_tx_default, lte_userPriority_max, lte_dataRate_default,
-            dataunit, dataunit_len, dest_mac);
+    sock = socket(AF_UNIX, SOCK_DGRAM, 0);
 
-    if (result == ar_success) {
-        printf("send success\n");
-        return 0;
-    } else {
-        printf("send fail(%d)\n", result);
-        return -1;
+    /* socket create error */
+    if (sock < 0) {
+        return;
     }
-} /* v2x_send_spdu */
+
+    memset(&addr_un, 0x00, sizeof(struct sockaddr_un));
+    addr_un.sun_family = AF_UNIX;
+    strcpy(addr_un.sun_path, UDS_FILE_ACU_RX);
+
+    sendto(sock, buffer, len, 0, (struct sockaddr *)&addr_un, sizeof(struct sockaddr_un));
+
+    close(sock);
+} /* v2x_send_to_acu */
 
 /**
  * @brief       v2x 송신 쓰레드
@@ -49,14 +50,56 @@ apiResult v2x_send_spdu(lte_dot3PsidRange psid, unsigned char *dataunit, int dat
  */
 void * v2x_tx_thread(void *param)
 {
-    int retval = 0;
+    int uds_sock = -1;
+    struct sockaddr_un msg_addr_un;
+    struct sockaddr_un msg_recv_un;
+    int namelen = sizeof(struct sockaddr_un);
     lte_dot3PsidRange psid = 0;
+    /* uds server open */
+    uds_sock = socket(AF_UNIX, SOCK_DGRAM, 0);
+
+    /* uds socket create error */
+    if (uds_sock < 0) {
+        sleep(1);
+        goto tx_thread_exit;
+    }
+
+    memset(&msg_addr_un, 0x00, sizeof(struct sockaddr_un));
+    msg_addr_un.sun_family = AF_UNIX;
+    strcpy(msg_addr_un.sun_path, UDS_FILE_ACU_TX);
+
+    if (uds_sock < 0) {
+        sleep(1);
+        goto tx_thread_exit;
+    }
+
+    if (bind(uds_sock, (struct sockaddr *) &msg_addr_un, sizeof(struct sockaddr_un)) < 0) {
+        sleep(1);
+        goto uds_server_exit;
+    }
+
+    /* set uds socket nonblocking */
+    if ((fl = fcntl(uds_sock, F_GETFL, NULL)) == -1) {
+        sleep(1);
+        goto uds_server_exit;
+    }
+
+    fl |= O_NONBLOCK;
+
+    if (fcntl(uds_sock, F_SETFL, fl) == -1) {
+        sleep(1);
+        goto uds_server_exit;
+    }
     printf("v2x_tx_thread start\n");
 
     while (1) {
-        sleep(1);
-    }
+        /* recv from acu interface */
+        retval = recvfrom(uds_sock, rx_buffer, sizeof(rx_buffer), 0, (struct sockaddr *)&msg_recv_un, (socklen_t*)&namelen);
 
+uds_server_exit:
+    if (uds_sock > 0) {
+        close(uds_sock);
+    }
     printf("v2x_tx_thread stop\n");
 
     pthread_exit(NULL);
@@ -85,6 +128,7 @@ void * v2x_rx_thread(void *param)
 
         if (retval > 0) {
             printf("rx : %d packet(psid %d)\n", retval, psid);
+            v2x_send_to_acu(buffer, retval);
         }
 
         sleep(1);
